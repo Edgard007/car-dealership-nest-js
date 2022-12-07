5aba88f49d40e27d0282ee8e7a0daac0b701cb3b
Edgardo Palacios <edgard07hp@gmail.com>
2022-12-06 20:52:24 -0600

    ✨ Fake CRUD for cars

--- src/cars/dto/update-car.dto.ts
+++ src/cars/dto/update-car.dto.ts
@@ -3,6 +3,10 @@ import { IsString, IsOptional } from 'class-validator';
 export class UpdateCarDTO {
   @IsString()
   @IsOptional()
+  readonly id?: number;
+
+  @IsString()
+  @IsOptional()
   readonly model?: string;
 
   @IsString()
